9710a1ee9becdf465665dfba1c6bc964b5021f6f
Enderxcthz <39433002+Enderxcthz@users.noreply.github.com>
2019-07-06 16:46:11 +0100

    Update XboxServices.py

--- XboxServices.py
+++ XboxServices.py
@@ -1,1 +1,26 @@
+## __   ___                _____                 _               
+## \ \ / / |              / ____|               (_)              
+##  \ V /| |__   _____  _| (___   ___ _ ____   ___  ___ ___  ___ 
+##   > < | '_ \ / _ \ \/ /\___ \ / _ \ '__\ \ / / |/ __/ _ \/ __|
+##  / . \| |_) | (_) >  < ____) |  __/ |   \ V /| | (_|  __/\__ \
+## /_/ \_\_.__/ \___/_/\_\_____/ \___|_|    \_/ |_|\___\___||___/
+## V1.0.0 ALPHA
+#///////Open source but you can still give credit. :(
+#/////// Credits to Endzy#3818 on discord. 
 
+import time
+import subprocess
+import webbrowser
+#import config
+import sys
+import os
+import datetime
+import math
+import cmath
+import cmd
+##############################################################################
+
+def TeredoReset():
+  openfile('teredo.bat')
+  print("Success! Teredo was reset.")
+  return
